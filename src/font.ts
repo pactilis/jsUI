9b5264d58896ae0aaa7d21e850266dac332e38ea
Jonatan Sombié <jonatan.sombie@gmail.com>
2020-08-21 17:33:15 +0200

    add font definition

--- src/font.ts
+++ src/font.ts
@@ -1,3 +1,5 @@
+import { View } from './view.js';
+
 export interface Font {
   fontFamily?: string;
   fontSize?: string;
@@ -11,6 +13,31 @@ export interface Font {
     | 'full-width'
     | 'full-size-kana';
 }
+
+View.prototype.font = function font({
+  fontSize,
+  fontWeight,
+  fontFamily,
+  letterSpacing,
+  textTransform,
+}: Font) {
+  if (fontSize) {
+    this.style('font-size', fontSize);
+  }
+  if (fontWeight) {
+    this.style('font-weight', fontWeight);
+  }
+  if (fontFamily) {
+    this.style('font-family', fontFamily);
+  }
+  if (letterSpacing) {
+    this.style('letter-spacing', letterSpacing);
+  }
+  if (textTransform) {
+    this.style('text-transform', textTransform);
+  }
+  return this;
+};
 
 declare module './view.js' {
   interface View {
