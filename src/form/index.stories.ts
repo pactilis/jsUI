5e57c3d0c9ad7e55e36924268616714a01ce514b
Jonatan Sombié <jonatan.sombie@pactilis.com>
2021-02-26 10:39:03 +0100

    add ability to reset form

--- src/form/index.stories.ts
+++ src/form/index.stories.ts
@@ -1,5 +1,9 @@
+/* eslint-disable import/no-duplicates */
+import '@material/mwc-textfield';
 import { html } from 'lit-html';
 import { styleMap } from 'lit-html/directives/style-map';
+import { view } from '../factory.js';
+import { useState } from '../hooks/index.js';
 import { HSTack } from '../layout/index.js';
 import { createView } from '../view.js';
 import { Form } from './index.js';
@@ -8,69 +12,117 @@ export default {
   title: 'form',
 };
 
-export const Simple = () =>
-  Form<{ name: string; gender: string }>(
-    ({ value, isValid, touched, onFieldChange, submit }) =>
-      HSTack(
-        createView(
-          ({ styles }) => html`
-            <div style="${styleMap(styles)}">
-              <label for="name">Name:</label>
-              <input
-                id="name"
-                type="text"
-                name="name"
-                required
-                .value="${value.name}"
-                @change="${(e: { target: HTMLInputElement }) =>
-                  onFieldChange(
-                    'name',
-                    e.target.value,
-                    e.target.checkValidity()
-                  )}"
-              />
-            </div>
-          `
-        ).color('blue'),
+const [MyForm] = view('demo-my-form', {
+  template() {
+    const [resetCount, setResetCount] = useState(0);
 
-        createView(
-          html`
-            <div>
-              <label for="gender">Gender:</label>
-              <select
-                id="gender"
-                .value="${value.gender}"
-                name="gender"
-                required
-                @blur="${(e: { target: HTMLSelectElement }) =>
-                  onFieldChange(
-                    'gender',
-                    e.target.value,
-                    e.target.checkValidity()
-                  )}"
-              >
-                <option value=""></option>
-                <option value="F">F</option>
-                <option value="M">M</option>
-              </select>
-            </div>
-          `
-        ),
+    return Form<{ name: string; lastname: string; gender: string }>(
+      ({ value, isValid, touched, onFieldChange, submit, setReset }) =>
+        HSTack(
+          createView(
+            ({ styles }) => html`
+              <div style="${styleMap(styles)}">
+                <label for="name">Name:</label>
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  required
+                  .value="${value.name}"
+                  @change="${(e: { target: HTMLInputElement }) =>
+                    onFieldChange(
+                      'name',
+                      e.target.value,
+                      e.target.checkValidity()
+                    )}"
+                />
+              </div>
+            `
+          ).color('blue'),
 
-        createView(
-          html`<button
-            ?disabled="${!isValid || !touched}"
-            type="submit"
-            @click="${submit}"
-          >
-            Submit
-          </button>`
-        )
-      ).justifyItems('end')
-  )
-    .value({ name: 'Jonatan', gender: 'F' })
-    .isValid({ name: true, gender: true })
-    .onChange((value, isValid) => console.log('form change', value, isValid))
-    .onSubmit((value, isValid) =>
-      console.log('submitting form', value, isValid)
-    ).body;
+          createView(html`
+            <mwc-textfield
+              label="Lastname"
+              required
+              .value="${value.lastname}"
+              @change="${(e: { target: any }) => {
+                onFieldChange(
+                  'lastname',
+                  e.target.value,
+                  e.target.checkValidity()
+                );
+              }}"
+              @blur="${(e: any) => {
+                setReset(
+                  'lastname',
+                  (() => {
+                    const field = e.target;
+                    return () => {
+                      console.log('resetting textfield', field);
+                      field.isUiValid = true;
+                      field.mdcFoundation.setValid(true);
+                    };
+                  })()
+                );
+              }}"
+            ></mwc-textfield>
+          `),
+
+          createView(
+            html`
+              <div>
+                <label for="gender">Gender:</label>
+                <select
+                  id="gender"
+                  .value="${value.gender}"
+                  name="gender"
+                  required
+                  @blur="${(e: { target: HTMLSelectElement }) => {
+                    onFieldChange(
+                      'gender',
+                      e.target.value,
+                      e.target.checkValidity()
+                    );
+                  }}"
+                >
+                  <option value=""></option>
+                  <option value="F">F</option>
+                  <option value="M">M</option>
+                </select>
+              </div>
+            `
+          ),
+
+          createView(
+            html`<button
+              type="button"
+              @click="${() => setResetCount(count => count + 1)}"
+            >
+              Reset
+            </button>`
+          ),
+
+          createView(
+            html`<button
+              ?disabled="${!isValid || !touched}"
+              type="submit"
+              @click="${submit}"
+            >
+              Submit
+            </button>`
+          )
+        ).justifyItems('end')
+    )
+      .value({ name: `Jonatan ${resetCount}`, gender: 'F', lastname: '' })
+      .resetDeps([resetCount])
+      .isValid({ name: true, gender: true, lastname: false })
+      .onChange((value, isValid) => console.log('form change', value, isValid))
+      .onSubmit((value, isValid) =>
+        console.log('submitting form', value, isValid)
+      )
+      .onResetting(() => console.log('Resetting form'))
+      .onResetted(() => console.log('Resetted form'));
+  },
+});
+
+export const Simple = () => MyForm().body;
--- src/form/index.ts
+++ src/form/index.ts
@@ -1,8 +1,29 @@
 import { html } from 'lit-html';
 import { Builder } from '../builder.js';
 import { view } from '../factory.js';
-import { useState } from '../hooks/index.js';
+import { useEffect, useState } from '../hooks/index.js';
 import { View } from '../view.js';
+
+export class FormProps<T> {
+  factory?: (_: FormFactoryParam<T>) => View = undefined;
+  value: T = {} as T;
+  isValid: { [key in keyof T]: boolean } = {} as { [key in keyof T]: boolean };
+  onChange?: (value: T, isValid: boolean) => void = undefined;
+  onSubmit?: (value: T, isValid: boolean) => void = undefined;
+  onResetting?: () => void = undefined;
+  onResetted?: () => void = undefined;
+  resetDeps: any = [];
+}
+
+export interface FormFactoryParam<T> {
+  onFieldChange: (name: keyof T, value: unknown, isValid: boolean) => void;
+  value: T;
+  touched: boolean;
+  isValid: boolean;
+  submit: () => void;
+  resetting: boolean;
+  setReset: (name: keyof T, fn: () => void) => void;
+}
 
 function formTemplate<T>({
   factory,
@@ -10,20 +31,29 @@ function formTemplate<T>({
   isValid: initialIsValid,
   onChange,
   onSubmit,
+  onResetting,
+  onResetted,
+  resetDeps,
 }: FormProps<T>) {
   const [value, setValue] = useState(initialValue);
   const [isValid, setIsValid] = useState(initialIsValid);
   const [touched, setTouched] = useState(false);
   const [formFieldChanged, setFormFieldChanged] = useState(false);
+  const [resetStore, setResetStore] = useState<{ [k in keyof T]: () => void }>(
+    {} as any
+  );
+  const [resetting, setResetting] = useState(false);
 
   const isFormValid = Object.keys(isValid).every(
     name => isValid[name as keyof T]
   );
 
-  if (touched && formFieldChanged) {
-    onChange?.(value, isFormValid);
-    setFormFieldChanged(false);
-  }
+  useEffect(() => {
+    if (touched && formFieldChanged) {
+      onChange?.(value, isFormValid);
+      setFormFieldChanged(false);
+    }
+  }, [touched, formFieldChanged]);
 
   const onFieldChange = (
     name: keyof T,
@@ -42,6 +72,34 @@ function formTemplate<T>({
     setFormFieldChanged(true);
   };
 
+  const setReset = (name: keyof T, fn: () => void) => {
+    setResetStore(prev => ({
+      ...prev,
+      [name]: fn,
+    }));
+  };
+
+  useEffect(() => {
+    setResetting(true);
+    setValue(initialValue);
+    setIsValid(initialIsValid);
+    setTouched(false);
+    setFormFieldChanged(false);
+
+    onResetting?.();
+  }, resetDeps);
+
+  useEffect(() => {
+    if (resetting) {
+      Object.keys(value).forEach(name => {
+        resetStore[name as keyof T]?.();
+      });
+      setResetting(false);
+      onResetted?.();
+      onChange?.(value, isFormValid);
+    }
+  }, [resetting]);
+
   return html`
     <form>
       ${factory?.({
@@ -50,25 +108,11 @@ function formTemplate<T>({
         isValid: isFormValid,
         touched,
         submit: () => onSubmit?.(value, isFormValid),
+        resetting,
+        setReset,
       }).body}
     </form>
   `;
-}
-
-export class FormProps<T> {
-  factory?: (_: FormFactoryParam<T>) => View = undefined;
-  value: T = {} as T;
-  isValid: { [key in keyof T]: boolean } = {} as { [key in keyof T]: boolean };
-  onChange?: (value: T, isValid: boolean) => void = undefined;
-  onSubmit?: (value: T, isValid: boolean) => void = undefined;
-}
-
-export interface FormFactoryParam<T> {
-  onFieldChange: (name: keyof T, value: unknown, isValid: boolean) => void;
-  value: T;
-  touched: boolean;
-  isValid: boolean;
-  submit: () => void;
 }
 
 export const [Form, FormView] = view('jsview-form', {
